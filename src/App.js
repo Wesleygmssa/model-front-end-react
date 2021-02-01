eaf8ddd7e20d943ccb93cd2fc044ee2ebecf3ed1
wesley guerra <guerra_ssa@hotmail.com>
2021-02-01 18:08:44 -0300

    header da aplicação ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Login from './pages/Login';
 import GlobalStyle from './styles/Global';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Header />
       <Login />
     </>
   );
